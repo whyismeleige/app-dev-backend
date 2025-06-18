05444c4f763ad59acadf67688ed810b74718b835
vyshnavi0907 <kvyshnavi099@gmail.com>
2025-06-18 14:18:33 +0530

    Routes Added

--- Utils/Database/dbConnect.js
+++ Utils/Database/dbConnect.js
@@ -12,4 +12,4 @@ const connectDB = async () => {
    }
 }
 
-connectDB();
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const db = require("./Utils/Database/dbConnect");
 const bodyParser = require("body-parser");
 const routes = require('./Routes/crud');
+const auth = require('./Routes/auth');
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json())
-
+app.use('/',auth);
 app.use('/',routes);
 
 app.listen(PORT, () => console.log("Server running on PORT:", PORT));
+
